26eaee1a728d5fdede40f36bd76986f5263c6340
Daniel Teixeira <dant25@gmail.com>
2019-08-14 15:54:19 -0300

    Usando notação #.

--- src/main.js
+++ src/main.js
@@ -7,17 +7,30 @@ import Contact from "./components/contact/contact";
 // Importando os components necessárias da lib react-materialize
 import { Container } from 'react-materialize';
 // Importanto o component <Switch /> e <Route /> da nossa Lib de rotas
-import { Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 
 const Main = () => (
     <main>
         <Container>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route path='/contact' component={Contact} />
-            </Switch>
+            <HashRouter>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/contact' component={Contact} />
+                    <Route component={NoMatch} />
+                </Switch>
+            </HashRouter>
         </Container>
     </main>
 );
+
+function NoMatch({ location }) {
+    return (
+        <div>
+            <h3>
+                No match for <code>{location.pathname}</code>
+            </h3>
+        </div>
+    );
+}
 
 export default Main;
